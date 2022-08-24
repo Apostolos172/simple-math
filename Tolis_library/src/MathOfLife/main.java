239df4233eab21b850fff2461c9a7f5dc6685690
Apostolos172 <apostolos.vad@gmail.com>
2022-08-24 20:12:23 +0300

    better testing code for main using methods

--- Tolis_library/src/MathOfLife/main.java
+++ Tolis_library/src/MathOfLife/main.java
@@ -5,11 +5,23 @@ import java.util.ArrayList;
 public class main {
 
 	public static void main(String[] args) {
-		ArrayList<Double> percentages = new ArrayList<>();
+		
+		// for testing purposes
+		testLibrary();
+	}
+
+	private static void testLibrary() {
+		ArrayList<Double> percentages = new ArrayList<Double>();
 		percentages.add(50.0);
 		percentages.add(6.0);
 		double price = FinalPrices.getFinalPriceHavingMultipleDiscounts(10, percentages, 30, 24);
-		System.out.println(price);
+		System.out.println(round4decimals(price));
+	}
+
+	private static double round4decimals(double price) {
+		// TODO Auto-generated method stub
+		
+		return Math.round(price*10000.0)/10000.0;
 	}
 
 }
